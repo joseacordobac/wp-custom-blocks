8bfb065241a3dc504a5b77d9715dc988ff483220
joseacordobac <joseacordobac@gmail.com>
2025-09-05 23:01:05 -0500

    termas configs

--- admin/CPT_tax_constructor/cpt-tax-register.php
+++ admin/CPT_tax_constructor/cpt-tax-register.php
@@ -5,9 +5,9 @@ require_once 'TaxonomiesConstructor.php';
 
 /** Create CPT **/
 // new CustomPostType(name, single name, related taxonomies, dash icon, type, register name, 'show in menu', has archive, 'rute name, position )
-new CustomPostTypeConstructor('Portafolio', 'portafolio', ['usos'], 'book-alt', ['post'], 'portafolio', false, true, 'portafolio', 10);
+//new CustomPostTypeConstructor('Portafolio', 'portafolio', ['usos, marcas'], 'book-alt', ['post'], 'portafolio', false, true, 'portafolio', 10);
 
 /** Taxonomies **/
 // new CustomTaxonomy(name, true, rewrite, register name, array(tipo de publicación));
-new TaxonomiesConstructor('Tipo de producto', true, 'tipo-producto', 'tipo-producto', ['portafolio']);
-new TaxonomiesConstructor('Marcas', true, 'marcas', 'marcas', ['portafolio']);
+//new TaxonomiesConstructor('Tipo de producto', true, 'tipo-producto', 'tipo-producto', ['portafolio']);
+//new TaxonomiesConstructor('Marcas', true, 'marcas', 'marcas', ['portafolio']);
